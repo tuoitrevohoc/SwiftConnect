bd80989ad349c61213abd73d91d9b31bc61e0c59
tuoitrevohoc <tuoitrevohoc@me.com>
2016-08-30 14:26:46 +0700

    Initial Commit

---
+++ SwiftConnect/SwiftConnect.h
@@ -0,0 +1,19 @@
+//
+//  SwiftConnect.h
+//  SwiftConnect
+//
+//  Created by Tran Thien Khiem on 8/30/16.
+//  Copyright © 2016 Tran Thien Khiem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftConnect.
+FOUNDATION_EXPORT double SwiftConnectVersionNumber;
+
+//! Project version string for SwiftConnect.
+FOUNDATION_EXPORT const unsigned char SwiftConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftConnect/PublicHeader.h>
+
+
